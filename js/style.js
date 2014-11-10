66ca5003cf6bea7f6454838ab225846cc4097c95
Johman10 <johveck@gmail.com>
2014-11-10 17:12:12 +0100

    Fade refresh button when scrolling in element

--- js/style.js
+++ js/style.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
   var panelWidth = $('.panel').outerWidth() * $('.panel').length;
   $('body').width(panelWidth);
+
+  $('.panel').on('core-header-transform', function(e) {
+    var refreshButton = $(this).find('.refresh_button');
+    var id = $(this).attr('id');
+    var opacity = $('#' + id + ' /deep/ #headerBg').css('opacity');
+    refreshButton.css('opacity', opacity);
+    // var d = e.currentTarget;
+    // console.log(refreshButton);
+    // var m = d.height() - d.condensedHeight;
+    // var scale = Math.max(0.75, (m - d.y) / (m / 0.25)  + 0.75);
+    // refreshButton.transform = refreshButton.webkitTransform =
+    //     'scale(' + scale + ') translateZ(0)';
+  });
 });
 
 // addEventListener('drag-start', function(e) {
