f305a3ae3273262f865e037c6d2d6997494ce320
Johman10 <johveck@gmail.com>
2014-11-28 19:45:26 +0100

    Sickbeard grooming + Force search - needs styling

--- js/sickbeard.js
+++ js/sickbeard.js
@@ -4,7 +4,6 @@
 $(document).ready(function() {
   chrome.storage.sync.get({
     SB_status: '',
-    SB_key: '',
     SB_address: '',
     SB_port: ''
   }, function(items) {
@@ -71,109 +70,79 @@ function sbShowData(SB_key, SB_address, SB_port) {
     // Episodes missed
     if (data.data.missed.length > 0) {
       $('.sb_missed').append('<h2>Missed</h2>');
-      $.each(data.data.missed, function(i) {
-        var tvdbid = data.data.missed[i].tvdbid;
-        var season = data.data.missed[i].season;
-        var episode = data.data.missed[i].episode;
-        var airdate = data.data.missed[i].airdate;
-        var showname = data.data.missed[i].show_name;
-        var date;
-        if (moment(airdate).year() > moment().year()) {
-          date = moment(airdate).format("MMM D, YYYY");
-        }
-        else {
-          date = moment(airdate).format("MMM D");
-        }
-        console.log(tvdbid, season, episode, airdate, showname, date);
-        var episodeString = " S" + (season<10?'0':'') + season + "E" + (episode<10?'0':'') + episode;
-        var searchApiUrl = url + "/?cmd=episode.search&tvdbid=" + tvdbid + "&season=" + season + "&episode=" + episode;
-        $('.sb_missed').append(
-          "<core-item label='" + showname + episodeString + "' class='sb_item'></core-item>" +
-          "<core-collapse opened=false class='sb_collapse'>" +
-            date +
-            "<a href='" + searchApiUrl + "'>Search</a>" +
-          "</core-collapse>"
-        );
-      });
+      listSeries(data, data.data.missed, ".sb_missed");
     }
 
     // Episodes today
     if (data.data.today.length > 0) {
       $('.sb_today').append('<h2>Today</h2>');
-      $.each(data.data.today, function(i) {
-        var tvdbid = data.data.today[i].tvdbid;
-        var season = data.data.today[i].season;
-        var episode = data.data.today[i].episode;
-        var airdate = data.data.today[i].airdate;
-        var showname = data.data.today[i].show_name;
-        var date;
-        if (moment(airdate).year() > moment().year()) {
-          date = moment(airdate).format("MMM D, YYYY");
-        }
-        else {
-          date = moment(airdate).format("MMM D");
-        }
-        var episodeString = " S" + (season<10?'0':'') + season + "E" + (episode<10?'0':'') + episode;
-        $('.sb_today').append(
-          "<core-item label='" + showname + episodeString + "' class='sb_item'></core-item>" +
-          "<core-collapse opened=false class='sb_collapse'>" +
-            date +
-          "</core-collapse>"
-        );
-      });
+      listSeries(data, data.data.today, ".sb_today");
     }
 
     // Episodes soon
     if (data.data.soon.length > 0) {
       $('.sb_soon').append('<h2>Soon</h2>');
-
-      $.each(data.data.soon, function(i) {
-        var tvdbid = data.data.soon[i].tvdbid;
-        var season = data.data.soon[i].season;
-        var episode = data.data.soon[i].episode;
-        var airdate = data.data.soon[i].airdate;
-        var showname = data.data.soon[i].show_name;
-        var date;
-        if (moment(airdate).year() > moment().year()) {
-          date = moment(data.data.soon[i].airdate).format("MMM D, YYYY");
-        }
-        else {
-          date = moment(airdate).format("MMM D");
-        }
-        var episodeString = " S" + (season<10?'0':'') + season + "E" + (episode<10?'0':'') + episode;
-        $('.sb_soon').append("<core-item label='" + showname + episodeString + "' class='sb_item'></core-item>" +
-          "<core-collapse opened=false class='sb_collapse'>" +
-            date +
-          "</core-collapse>"
-        );
-      });
+      listSeries(data, data.data.soon, ".sb_soon");
     }
 
     // Episodes later
     if (data.data.later.length > 0) {
       $('.sb_later').append('<h2>Later</h2>');
-
-      $.each(data.data.later, function(i) {
-        var tvdbid = data.data.later[i].tvdbid;
-        var season = data.data.later[i].season;
-        var episode = data.data.later[i].episode;
-        var airdate = data.data.later[i].airdate;
-        var showname = data.data.later[i].show_name;
-        var date;
-        if (moment(airdate).year() > moment().year()) {
-          date = moment(airdate).format("MMM D, YYYY");
-        }
-        else {
-          date = moment(airdate).format("MMM D");
-        }
-        var episodeString = " S" + (season<10?'0':'') + season + "E" + (episode<10?'0':'') + episode;
-        $('.sb_later').append(
-          "<core-item label='" + showname + episodeString + "' class='sb_item'></core-item>" +
-          "<core-collapse opened=false class='sb_collapse'>" +
-            date +
-          "</core-collapse>"
-        );
-      });
+      listSeries(data, data.data.later, ".sb_later");
     }
+
+    $('.searchEpisode').click(function() {
+      searchEpisode($(this));
+    });
   }
+}
+
+function listSeries(data, query, parent) {
+  $.each(query, function(i, episodeData) {
+    var tvdbid = episodeData.tvdbid,
+        season = episodeData.season,
+        episode = episodeData.episode,
+        airdate = episodeData.airdate,
+        showname = episodeData.show_name,
+        date;
+
+    if (moment(airdate).year() > moment().year()) {
+      date = moment(airdate).format("MMM D, YYYY");
+    }
+    else {
+      date = moment(airdate).format("MMM D");
+    }
+    var episodeString = " S" + (season<10?'0':'') + season + "E" + (episode<10?'0':'') + episode;
+    $(parent).append(
+      "<core-item label='" + showname + episodeString + "' class='sb_item'></core-item>" +
+      "<core-collapse opened=false class='sb_collapse'>" +
+        date +
+        "<div class='searchEpisode " + tvdbid + "'>Search</div>" +
+      "</core-collapse>"
+    );
+
+    $('.' + tvdbid).data("episode", { tvdbid: tvdbid, season: season, episode: episode });
+  });
+}
+
+function searchEpisode(clickedObject) {
+  console.log('click');
+  chrome.storage.sync.get({
+    SB_key: '',
+    SB_address: '',
+    SB_port: ''
+  }, function(items) {
+    var url = items.SB_address + ":" + items.SB_port  + "/api/" + items.SB_key;
+    var searchApiUrl = url + "/?cmd=episode.search&tvdbid=" + clickedObject.data("episode").tvdbid + "&season=" + clickedObject.data("episode").season + "&episode=" + clickedObject.data("episode").episode;
+
+    $.ajax({
+      url: searchApiUrl,
+      success: function(data) {
+        console.log(data);
+      },
+      error: function() {
+        console.log("error");
+      }
+    });
+  });
 }
