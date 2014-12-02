8134ad80c1cd861ada7f922f8a56ff9f7360d2cc
Johman10 <johveck@gmail.com>
2014-12-02 13:24:42 +0100

    Styling improvements

--- js/style.js
+++ js/style.js
@@ -4,15 +4,19 @@ $(document).ready(function() {
 
   $('.panel').on('core-header-transform', function(e) {
     var refreshButton = $(this).find('.refresh_button');
+    var refreshLoading = $(this).find('.refresh_loading_icon');
     var id = $(this).attr('id');
     var opacity = $('#' + id + ' /deep/ #headerBg').css('opacity');
     refreshButton.css('opacity', opacity);
+    refreshLoading.css('opacity', opacity);
 
     if (opacity < 0.01) {
       refreshButton.hide();
+      refreshLoading.hide();
     }
     else {
       refreshButton.show();
+      refreshLoading.show();
     }
   });
 
