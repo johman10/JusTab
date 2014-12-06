3f0c9c81217f097807537f2b78b7f47e34df1f3e
Johman10 <johveck@gmail.com>
2014-12-06 22:17:07 +0100

    Change service checkbox in settings to toggle button

--- js/options.js
+++ js/options.js
@@ -43,39 +43,41 @@ function save_options() {
     }
   });
 
+  console.log($('paper-toggle-button[name=GC_status]').attr('aria-pressed'));
+
   formatUrl('FB_url');
   formatUrl('CP_address');
   formatUrl('SB_address');
   formatUrl('SAB_address');
 
   chrome.storage.sync.set({
-    GC_status: $('paper-checkbox[name=GC_status]').attr('aria-checked') == "true" ? true : false,
+    GC_status: $('paper-toggle-button[name=GC_status]').attr('aria-pressed') == "true" ? true : false,
     calendars: calendars,
     GC_refresh: $('#GC_refresh').val(),
-    GM_status: $('paper-checkbox[name=GM_status]').attr('aria-checked') == "true" ? true : false,
+    GM_status: $('paper-toggle-button[name=GM_status]').attr('aria-pressed') == "true" ? true : false,
     GM_email: $('#GM_email').val(),
     GM_refresh: $('#GM_refresh').val(),
-    FB_status: $('paper-checkbox[name=FB_status]').attr('aria-checked') == "true" ? true : false,
+    FB_status: $('paper-toggle-button[name=FB_status]').attr('aria-pressed') == "true" ? true : false,
     FB_url: FB_url.var,
     FB_refresh: $('#FB_refresh').val(),
-    CP_status: $('paper-checkbox[name=CP_status]').attr('aria-checked') == "true" ? true : false,
+    CP_status: $('paper-toggle-button[name=CP_status]').attr('aria-pressed') == "true" ? true : false,
     CP_address: CP_address.var,
     CP_port: $('#CP_port').val(),
     CP_key: $('#CP_key').val(),
     CP_refresh: $('#CP_refresh').val(),
-    SB_status: $('paper-checkbox[name=SB_status]').attr('aria-checked') == "true" ? true : false,
+    SB_status: $('paper-toggle-button[name=SB_status]').attr('aria-pressed') == "true" ? true : false,
     SB_address: SB_address.var,
     SB_port: $('#SB_port').val(),
     SB_key: $('#SB_key').val(),
     SB_refresh: $('#SB_refresh').val(),
-    SAB_status: $('paper-checkbox[name=SAB_status]').attr('aria-checked') == "true" ? true : false,
+    SAB_status: $('paper-toggle-button[name=SAB_status]').attr('aria-pressed') == "true" ? true : false,
     SAB_address: SAB_address.var,
     SAB_port: $('#SAB_port').val(),
     SAB_key: $('#SAB_key').val(),
     SAB_history: $('#SAB_history').val(),
     SABQ_refresh: $('#SABQ_refresh').val(),
     SABH_refresh: $('#SABH_refresh').val(),
-    DN_status: $('paper-checkbox[name=DN_status]').attr('aria-checked') == "true" ? true : false,
+    DN_status: $('paper-toggle-button[name=DN_status]').attr('aria-pressed') == "true" ? true : false,
     DN_refresh: $('#DN_refresh').val(),
   }, function() {
     // Update status to let user know options were saved.
@@ -117,32 +119,32 @@ function restore_options() {
     DN_status: '',
     DN_refresh: '15'
   }, function(items) {
-    $('paper-checkbox[name=GC_status]').attr('checked', items.GC_status);
+    $('paper-toggle-button[name=GC_status]').attr('checked', items.GC_status);
     $('#GC_refresh').val(items.GC_refresh);
-    $('paper-checkbox[name=GM_status]').attr('checked', items.GM_status);
+    $('paper-toggle-button[name=GM_status]').attr('checked', items.GM_status);
     $('#GM_email').val(items.GM_email);
     $('#GM_refresh').val(items.GM_refresh);
-    $('paper-checkbox[name=FB_status]').attr('checked', items.FB_status);
+    $('paper-toggle-button[name=FB_status]').attr('checked', items.FB_status);
     $('#FB_url').val(items.FB_url);
     $('#FB_refresh').val(items.FB_refresh);
-    $('paper-checkbox[name=CP_status]').attr('checked', items.CP_status);
+    $('paper-toggle-button[name=CP_status]').attr('checked', items.CP_status);
     $('#CP_address').val(items.CP_address);
     $('#CP_port').val(items.CP_port);
     $('#CP_key').val(items.CP_key);
     $('#CP_refresh').val(items.CP_refresh);
-    $('paper-checkbox[name=SB_status]').attr('checked', items.SB_status);
+    $('paper-toggle-button[name=SB_status]').attr('checked', items.SB_status);
     $('#SB_address').val(items.SB_address);
     $('#SB_port').val(items.SB_port);
     $('#SB_key').val(items.SB_key);
     $('#SB_refresh').val(items.SB_refresh);
-    $('paper-checkbox[name=SAB_status]').attr('checked', items.SAB_status);
+    $('paper-toggle-button[name=SAB_status]').attr('checked', items.SAB_status);
     $('#SAB_address').val(items.SAB_address);
     $('#SAB_port').val(items.SAB_port);
     $('#SAB_key').val(items.SAB_key);
     $('#SAB_history').val(items.SAB_history);
     $('#SABQ_refresh').val(items.SABQ_refresh);
     $('#SABH_refresh').val(items.SABH_refresh);
-    $('paper-checkbox[name=DN_status]').attr('checked', items.DN_status);
+    $('paper-toggle-button[name=DN_status]').attr('checked', items.DN_status);
     $('#DN_refresh').val(items.DN_refresh);
   });
 }
--- options_build.js
+++ options_build.js
@@ -8677,3 +8677,79 @@ Polymer('core-pages');;
 
   });
 
+;
+
+
+  Polymer('paper-toggle-button', {
+    
+    /**
+     * Fired when the checked state changes due to user interaction.
+     *
+     * @event change
+     */
+     
+    /**
+     * Fired when the checked state changes.
+     *
+     * @event core-change
+     */
+
+    /**
+     * Gets or sets the state, `true` is checked and `false` is unchecked.
+     *
+     * @attribute checked
+     * @type boolean
+     * @default false
+     */
+    checked: false,
+    
+    /**
+     * If true, the toggle button is disabled.  A disabled toggle button cannot
+     * be tapped or dragged to change the checked state.
+     *
+     * @attribute disabled
+     * @type boolean
+     * @default false
+     */
+    disabled: false,
+
+    trackStart: function(e) {
+      this._w = this.$.toggleBar.offsetLeft + this.$.toggleBar.offsetWidth;
+      e.preventTap();
+    },
+
+    trackx: function(e) {
+      this._x = Math.min(this._w, 
+          Math.max(0, this.checked ? this._w + e.dx : e.dx));
+      this.$.toggleRadio.classList.add('dragging');
+      var s =  this.$.toggleRadio.style;
+      s.webkitTransform = s.transform = 'translate3d(' + this._x + 'px,0,0)';
+    },
+
+    trackEnd: function() {
+      var s =  this.$.toggleRadio.style;
+      s.transform = s.webkitTransform = '';
+      this.$.toggleRadio.classList.remove('dragging');
+      var old = this.checked;
+      this.checked = Math.abs(this._x) > this._w / 2;
+      if (this.checked !== old) {
+        this.fire('change');
+      }
+    },
+    
+    checkedChanged: function() {
+      this.setAttribute('aria-pressed', Boolean(this.checked));
+      this.fire('core-change');
+    },
+    
+    changeAction: function(e) {
+      e.stopPropagation();
+      this.fire('change');
+    },
+    
+    stopPropagation: function(e) {
+      e.stopPropagation();
+    }
+
+  });
+
