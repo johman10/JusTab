ab5f9f2079fd96a54dc6cd48d7241a0b2e3f02bd
Johman10 <johveck@gmail.com>
2014-12-11 18:54:35 +0100

    Background hover color fix Sickbeard

--- js/background/background.js
+++ js/background/background.js
@@ -15,26 +15,26 @@ chrome.storage.sync.get({
   DN_status: '',
   DN_refresh: ''
 }, function(items) {
-  if (items.GC_status === true) {
+  if (items.GC_status == true) {
     chrome.alarms.create('googleCalendar', {periodInMinutes: parseFloat(items.GC_refresh)});
   }
-  if (items.GM_status === true) {
+  if (items.GM_status == true) {
     chrome.alarms.create('gmail', {periodInMinutes: parseFloat(items.GM_refresh)});
   }
-  if (items.FB_status === true) {
+  if (items.FB_status == true) {
     chrome.alarms.create('facebook', {periodInMinutes: parseFloat(items.FB_refresh)});
   }
-  if (items.CP_status === true) {
+  if (items.CP_status == true) {
     chrome.alarms.create('couchPotato', {periodInMinutes: parseFloat(items.CP_refresh)});
   }
-  if (items.SB_status === true) {
+  if (items.SB_status == true) {
     chrome.alarms.create('sickBeard', {periodInMinutes: parseFloat(items.SB_refresh)});
   }
-  if (items.SAB_status === true) {
+  if (items.SAB_status == true) {
     chrome.alarms.create('sabnzbdQueue', {periodInMinutes: parseFloat(items.SABQ_refresh)});
     chrome.alarms.create('sabnzbdHistory', {periodInMinutes: parseFloat(items.SABH_refresh)});
   }
-  if (items.DN_status === true) {
+  if (items.DN_status == true) {
     chrome.alarms.create('designerNews', {periodInMinutes: parseFloat(items.DN_refresh)});
   }
 });
@@ -49,26 +49,33 @@ chrome.runtime.onStartup.addListener(function() {
     SAB_status: '',
     DN_status: ''
   }, function(items) {
-    if (items.GC_status === true) {
+    if (items.GC_status == true) {
+      console.log('Startup GC_status');
       getCalendarData();
     }
-    if (items.GM_status === true) {
+    if (items.GM_status == true) {
+      console.log('Startup GM_status');
       getGmailData();
     }
-    if (items.FB_status === true) {
+    if (items.FB_status == true) {
+      console.log('Startup FB_status');
       getFacebookData();
     }
-    if (items.CP_status === true) {
+    if (items.CP_status == true) {
+      console.log('Startup CP_status');
       getCouchpotatoData();
     }
-    if (items.SB_status === true) {
+    if (items.SB_status == true) {
+      console.log('Startup SB_status');
       getSickbeardData();
     }
-    if (items.SAB_status === true) {
+    if (items.SAB_status == true) {
+      console.log('Startup SAB_status');
       getSabnzbdHistory();
       getSabnzbdQueue();
     }
-    if (items.DN_status === true) {
+    if (items.DN_status == true) {
+      console.log('Startup DN_status');
       getDesignerNewsData();
     }
   });
@@ -76,27 +83,35 @@ chrome.runtime.onStartup.addListener(function() {
 
 chrome.alarms.onAlarm.addListener(function(alarm) {
   if (alarm.name == 'googleCalendar') {
+    console.log(alarm.name);
     getCalendarData();
   }
   else if (alarm.name == 'gmail') {
+    console.log(alarm.name);
     getGmailData();
   }
   else if (alarm.name == 'facebook') {
+    console.log(alarm.name);
     getFacebookData();
   }
   else if (alarm.name == 'couchPotato') {
+    console.log(alarm.name);
     getCouchPotatoData();
   }
   else if (alarm.name == 'sickBeard') {
+    console.log(alarm.name);
     getSickBeardData();
   }
   else if (alarm.name == 'sabnzbdQueue') {
+    console.log(alarm.name);
     getSabnzbdQueue();
   }
   else if (alarm.name == 'sabnzbdHistory') {
+    console.log(alarm.name);
     getSabnzbdHistory();
   }
   else if (alarm.name == 'designerNews') {
+    console.log(alarm.name);
     getDesignerNewsData();
   }
 });
