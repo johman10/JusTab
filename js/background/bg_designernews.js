3c63561e24ef2598eaccc6b7ab961c3137c79f1b
Johman10 <johveck@gmail.com>
2015-01-24 23:31:45 +0100

    Designernews prep for upvoting

--- js/background/bg_designernews.js
+++ js/background/bg_designernews.js
@@ -6,6 +6,25 @@ function getDesignerNewsData(callback) {
   var apiCall = "stories";
   var apiKey = "?client_id=e7c9f9422feb744c661cc25a248d3b7206962f0605e174ae30aab12a05fb107a";
 
+  // $.ajax({
+  //   url: 'https://api-news.layervault.com/oauth/token',
+  //   data: {
+  //     username: - User E-mail,
+  //     password: - User Password,
+  //     client_id: 'e7c9f9422feb744c661cc25a248d3b7206962f0605e174ae30aab12a05fb107a',
+  //     client_secret: '64945f7fefea5e0fb33fe064a053cc7375286ef32589b3b3367c7b339fe6fbe4',
+  //     redirect_uri: window.location.href,
+  //     grant_type: "password"
+  //   },
+  //   type: 'POST',
+  //   success: function(data){
+  //       console.log(data);
+  //   },
+  //   error: function(xhr, ajaxOptions, thrownError){
+  //       console.log(xhr, ajaxOptions, thrownError);
+  //   }
+  // });
+
   $.when($.ajax({
     url: url + apiCall + apiKey,
     dataType: 'json',
@@ -34,6 +53,7 @@ function dnHTML() {
     var dn_links = '';
 
     $.each(data.stories, function(i, story) {
+      console.log('/api/v1/stories/' + story.id + '/upvote');
       dn_links +=
         '<core-item class="dn_link_container">' +
           '<a href="' + story.url + '" class="dn_story_url" target="_blank">' +
