35dc6055a117cdf7c01cf924a37e47b8b3d66ac3
Johman10 <johveck@gmail.com>
2015-01-31 23:19:35 +0100

    SB change to serviceData

--- js/background/bg_sickbeard.js
+++ js/background/bg_sickbeard.js
@@ -2,138 +2,122 @@
 // http://sickbeard.com/api/
 
 function getSickBeardData(callback) {
-  SBChromeData(function(items) {
-    var url = items.SB_address + ":" + items.SB_port + "/";
-    var apiKey = "api/" + items.SB_key + "/";
-    var apiCall = "?cmd=future&sort=date&type=today|missed|soon|later";
+  var url = serviceData.SB.apiUrl;
+  var apiCall = "?cmd=future&sort=date&type=today|missed|soon|later";
 
-    $.when($.ajax({
-      url: url + apiKey + apiCall,
-      dataType: 'json',
-      async: false,
-      timeout: 3000,
-      success: function(data) {
-        localStorage.setItem("Sickbeard_error", false);
-        localStorage.setItem("Sickbeard", JSON.stringify(data));
-        sbHTML();
-      },
-      error: function(xhr, ajaxOptions, thrownError) {
-        console.log(xhr, ajaxOptions, thrownError);
-        localStorage.setItem("Sickbeard_error", true);
-      }
-    })).then(function() {
-      if (callback) {
-        callback();
-      }
-    });
-  });
-}
-
-function sbHTML() {
-  SBChromeData(function(items) {
-    var url = items.SB_address + ":" + items.SB_port  + "/api/" + items.SB_key;
-
-    if (localStorage.Sickbeard) {
-      data = JSON.parse(localStorage.getItem('Sickbeard'));
-
-      // Episodes missed
-      if (data.data.missed.length > 0) {
-        var SB_missed = '<h2>Missed</h2>';
-        listSeries(data.data.missed, SB_missed, 'SickbeardMissedHTML');
-      } else {
-        localStorage.removeItem('SickbeardMissedHTML');
-      }
-
-      // Episodes today
-      if (data.data.today.length > 0) {
-        var SB_today = '<h2>Today</h2>';
-        listSeries(data.data.today, SB_today, 'SickbeardTodayHTML');
-      } else {
-        localStorage.removeItem('SickbeardTodayHTML');
-      }
-
-      // Episodes soon
-      if (data.data.soon.length > 0) {
-        var SB_soon = '<h2>Soon</h2>';
-        listSeries(data.data.soon, SB_soon, 'SickbeardSoonHTML');
-      } else {
-        localStorage.removeItem('SickbeardSoonHTML');
-      }
-
-      // Episodes later
-      if (data.data.later.length > 0) {
-        var SB_later = '<h2>Later</h2>';
-        listSeries(data.data.later, SB_later, 'SickbeardLaterHTML');
-      } else {
-        localStorage.removeItem('SickbeardLaterHTML');
-      }
+  $.when($.ajax({
+    url: url + apiCall,
+    dataType: 'json',
+    async: false,
+    timeout: 3000,
+    success: function(data) {
+      localStorage.setItem("Sickbeard_error", false);
+      localStorage.setItem("Sickbeard", JSON.stringify(data));
+      sbHTML();
+    },
+    error: function(xhr, ajaxOptions, thrownError) {
+      console.log(xhr, ajaxOptions, thrownError);
+      localStorage.setItem("Sickbeard_error", true);
+    }
+  })).then(function() {
+    if (callback) {
+      callback();
     }
   });
 }
 
-function listSeries(query, HTML, storageName) {
-  SBChromeData(function(items) {
-    $.each(query, function(i, episodeData) {
-      var tvdbid = episodeData.tvdbid,
-          season = episodeData.season,
-          episode = episodeData.episode,
-          airdate = episodeData.airdate,
-          showname = episodeData.show_name,
-          date;
+function sbHTML() {
+  var url = serviceData.SB.apiUrl;
 
-      posterUrl = items.SB_address + ":" + items.SB_port + "/api/" + items.SB_key + "/?cmd=show.getposter&tvdbid=" + tvdbid;
+  if (serviceData.SB.JSON) {
+    data = serviceData.SB.JSON;
 
-      if (moment(airdate).isSame(moment(), 'day')) {
-        date = 'Today';
-      }
-      else if (moment(airdate).isSame(moment().add(1, 'day'), 'day')) {
-        date = 'Tomorrow';
-      }
-      else if (moment(airdate).isSame(moment().subtract(1, 'day'), 'day')) {
-        date = 'Yesterday';
-      }
-      else if (moment(airdate).year() > moment().year()) {
-        date = moment(airdate).format("MMM D, YYYY");
-      }
-      else {
-        date = moment(airdate).format("MMM D");
-      }
-      var episodeString = " S" + (season<10?'0':'') + season + "E" + (episode<10?'0':'') + episode;
+    // Episodes missed
+    if (data.data.missed.length > 0) {
+      var SB_missed = '<h2>Missed</h2>';
+      listSeries(data.data.missed, SB_missed, 'SickbeardMissedHTML');
+    } else {
+      localStorage.removeItem('SickbeardMissedHTML');
+    }
 
-      HTML +=
-        "<core-item label='" + showname + episodeString + "' class='sb_item'>" +
-          "<div class='sb_poster_container'>" +
-            "<img class='sb_poster' src='img/poster_fallback.png' data-src='" + posterUrl+ "'></core-image>" +
-          "</div>" +
-          "<div class='sb_collapse_icon_container'>" +
-            "<core-icon class='sb_collapse_icon' icon='expand-more'></core-icon>" +
-          "</div>" +
-        "</core-item>" +
-        "<core-collapse opened=false class='sb_collapse'>" +
-          "<core-item>" +
-            date +
-            "<div class='sb_collapse_buttons'>" +
-              "<paper-icon-button class='sb_search_episode' data-tvdbid='" + tvdbid + "' data-season='" + season + "' data-episode='" + episode + "' icon='search'>Search</paper-icon-button>" +
-              "<paper-spinner class='sb_search_spinner'></paper-spinner>" +
-              "<a class='sb_tvdb_link' href='http://thetvdb.com/?tab=series&id=" + tvdbid + "' target='_blank'>" +
-                "<paper-icon-button class='sb_tvdb_link_icon' icon='info-outline'></paper-icon-button>" +
-              "</a>" +
-              "<paper-icon-button class='sb_mark_episode' data-tvdbid='" + tvdbid + "' data-season='" + season + "' data-episode='" + episode + "' icon='done'>Mark Done</paper-icon-button>" +
-              "</div>" +
-          "</core-item>" +
-        "</core-collapse>"
-      ;
-    });
+    // Episodes today
+    if (data.data.today.length > 0) {
+      var SB_today = '<h2>Today</h2>';
+      listSeries(data.data.today, SB_today, 'SickbeardTodayHTML');
+    } else {
+      localStorage.removeItem('SickbeardTodayHTML');
+    }
 
-    localStorage.setItem(storageName, HTML);
-  });
+    // Episodes soon
+    if (data.data.soon.length > 0) {
+      var SB_soon = '<h2>Soon</h2>';
+      listSeries(data.data.soon, SB_soon, 'SickbeardSoonHTML');
+    } else {
+      localStorage.removeItem('SickbeardSoonHTML');
+    }
+
+    // Episodes later
+    if (data.data.later.length > 0) {
+      var SB_later = '<h2>Later</h2>';
+      listSeries(data.data.later, SB_later, 'SickbeardLaterHTML');
+    } else {
+      localStorage.removeItem('SickbeardLaterHTML');
+    }
+  }
 }
 
-function SBChromeData(callback) {
-  chrome.storage.sync.get({
-    SB_status: '',
-    SB_address: '',
-    SB_port: '',
-    SB_key: ''
-  }, callback);
+function listSeries(query, HTML, storageName) {
+  $.each(query, function(i, episodeData) {
+    var tvdbid = episodeData.tvdbid,
+        season = episodeData.season,
+        episode = episodeData.episode,
+        airdate = episodeData.airdate,
+        showname = episodeData.show_name,
+        date;
+
+    posterUrl = serviceData.SB.apiUrl + "/?cmd=show.getposter&tvdbid=" + tvdbid;
+
+    if (moment(airdate).isSame(moment(), 'day')) {
+      date = 'Today';
+    }
+    else if (moment(airdate).isSame(moment().add(1, 'day'), 'day')) {
+      date = 'Tomorrow';
+    }
+    else if (moment(airdate).isSame(moment().subtract(1, 'day'), 'day')) {
+      date = 'Yesterday';
+    }
+    else if (moment(airdate).year() > moment().year()) {
+      date = moment(airdate).format("MMM D, YYYY");
+    }
+    else {
+      date = moment(airdate).format("MMM D");
+    }
+    var episodeString = " S" + (season<10?'0':'') + season + "E" + (episode<10?'0':'') + episode;
+
+    HTML +=
+      "<core-item label='" + showname + episodeString + "' class='sb_item'>" +
+        "<div class='sb_poster_container'>" +
+          "<img class='sb_poster' src='img/poster_fallback.png' data-src='" + posterUrl+ "'></core-image>" +
+        "</div>" +
+        "<div class='sb_collapse_icon_container'>" +
+          "<core-icon class='sb_collapse_icon' icon='expand-more'></core-icon>" +
+        "</div>" +
+      "</core-item>" +
+      "<core-collapse opened=false class='sb_collapse'>" +
+        "<core-item>" +
+          date +
+          "<div class='sb_collapse_buttons'>" +
+            "<paper-icon-button class='sb_search_episode' data-tvdbid='" + tvdbid + "' data-season='" + season + "' data-episode='" + episode + "' icon='search'>Search</paper-icon-button>" +
+            "<paper-spinner class='sb_search_spinner'></paper-spinner>" +
+            "<a class='sb_tvdb_link' href='http://thetvdb.com/?tab=series&id=" + tvdbid + "' target='_blank'>" +
+              "<paper-icon-button class='sb_tvdb_link_icon' icon='info-outline'></paper-icon-button>" +
+            "</a>" +
+            "<paper-icon-button class='sb_mark_episode' data-tvdbid='" + tvdbid + "' data-season='" + season + "' data-episode='" + episode + "' icon='done'>Mark Done</paper-icon-button>" +
+            "</div>" +
+        "</core-item>" +
+      "</core-collapse>"
+    ;
+  });
+
+  localStorage.setItem(storageName, HTML);
 }
--- js/sickbeard.js
+++ js/sickbeard.js
@@ -1,170 +1,152 @@
 // Docs:
 // http://sickbeard.com/api
 
-SBChromeData(function(items) {
-  $(document).ready(function() {
-    if (items.SB_status === true) {
-      sbShowData();
+serviceDataDone.done(function() {
+  if (serviceData.SB.status) {
+    window[serviceData.SB.feFunctionName]();
 
-      $('#sickbeard .refresh_sb').click(function(event) {
-        if ($('#sickbeard .error:visible')) {
-          $('#sickbeard .error:visible').slideUp(400);
-        }
-        $('#sickbeard .refresh_sb').fadeOut(400, function() {
-          $('.loading_sb').attr('active', true);
-          chrome.runtime.getBackgroundPage(function(backgroundPage) {
-            backgroundPage.getSickBeardData(function() {
-              $('.loading_sb').attr('active', false);
-              setTimeout(function() {
-                $('#sickbeard .refresh_sb').fadeIn(400);
-              }, 400);
-            });
+    $('#sickbeard .refresh_sb').click(function(event) {
+      if ($('#sickbeard .error:visible')) {
+        $('#sickbeard .error:visible').slideUp(400);
+      }
+      $('#sickbeard .refresh_sb').fadeOut(400, function() {
+        $('.loading_sb').attr('active', true);
+        chrome.runtime.getBackgroundPage(function(backgroundPage) {
+          backgroundPage.getSickBeardData(function() {
+            $('.loading_sb').attr('active', false);
+            setTimeout(function() {
+              $('#sickbeard .refresh_sb').fadeIn(400);
+            }, 400);
           });
         });
       });
+    });
 
-      $('#sickbeard core-toolbar a').attr('href', items.SB_address + ':' + items.SB_port);
+    $('#sickbeard core-toolbar a').attr('href', serviceData.SB.url);
 
-      $('#sickbeard, .sickbeard_info').show();
-      $('body').width($('body').width() + $('#sickbeard').width());
-      $('.bottom_bar_container').width($('.panel_container').width());
-    }
-  });
+    $('#sickbeard, .sickbeard_info').show();
+    $('body').width($('body').width() + $('#sickbeard').width());
+    $('.bottom_bar_container').width($('.panel_container').width());
+  }
+});
 
-  $("html").on('click', ".sb_item", function(event) {
-    var collapseItem = $(this).next('.sb_collapse');
-    var collapseIcon = $(this).find('.sb_collapse_icon');
-    if (collapseItem.attr('opened') == 'false') {
-      $('.sb_collapse').attr('opened', false);
-      $('.sb_item').css('background-color', '#fafafa');
-      $('.sb_collapse_icon_container').css('background-color', '#fafafa');
-      $('.sb_collapse_icon[icon=expand-less]').fadeOut(165, function() {
-        $(this).attr('icon', 'expand-more');
-        $(this).fadeIn(165);
-      });
-      $(this).css('background-color', '#eee');
-      collapseIcon.parent().css('background-color', '#eee');
-      collapseItem.attr('opened', true);
-      collapseIcon.fadeOut(165, function() {
-        collapseIcon.attr('icon', 'expand-less');
-        collapseIcon.fadeIn(165);
-      });
-    }
-    else {
-      $(this).css('background-color', '#fafafa');
-      collapseIcon.parent().css('background-color', '#fafafa');
-      collapseItem.attr('opened', false);
-      collapseIcon.fadeOut(165, function() {
-        collapseIcon.attr('icon', 'expand-more');
-        collapseIcon.fadeIn(165);
-      });
-    }
-  });
+$("html").on('click', ".sb_item", function(event) {
+  var collapseItem = $(this).next('.sb_collapse');
+  var collapseIcon = $(this).find('.sb_collapse_icon');
+  if (collapseItem.attr('opened') == 'false') {
+    $('.sb_collapse').attr('opened', false);
+    $('.sb_item').css('background-color', '#fafafa');
+    $('.sb_collapse_icon_container').css('background-color', '#fafafa');
+    $('.sb_collapse_icon[icon=expand-less]').fadeOut(165, function() {
+      $(this).attr('icon', 'expand-more');
+      $(this).fadeIn(165);
+    });
+    $(this).css('background-color', '#eee');
+    collapseIcon.parent().css('background-color', '#eee');
+    collapseItem.attr('opened', true);
+    collapseIcon.fadeOut(165, function() {
+      collapseIcon.attr('icon', 'expand-less');
+      collapseIcon.fadeIn(165);
+    });
+  }
+  else {
+    $(this).css('background-color', '#fafafa');
+    collapseIcon.parent().css('background-color', '#fafafa');
+    collapseItem.attr('opened', false);
+    collapseIcon.fadeOut(165, function() {
+      collapseIcon.attr('icon', 'expand-more');
+      collapseIcon.fadeIn(165);
+    });
+  }
+});
 
-  $("body").on('click', ".sb_search_episode", function(event) {
-    searchEpisode($(this));
-  });
+$("body").on('click', ".sb_search_episode", function(event) {
+  searchEpisode($(this));
+});
 
-  $("body").on('click', ".sb_mark_episode", function(event) {
-    markEpisode($(this));
-  });
+$("body").on('click', ".sb_mark_episode", function(event) {
+  markEpisode($(this));
 });
 
 function sbShowData() {
-  SBChromeData(function(items) {
-    $('#sickbeard .sb_missed').empty();
-    $('#sickbeard .sb_today').empty();
-    $('#sickbeard .sb_soon').empty();
-    $('#sickbeard .sb_later').empty();
+  $('#sickbeard .sb_missed').empty();
+  $('#sickbeard .sb_today').empty();
+  $('#sickbeard .sb_soon').empty();
+  $('#sickbeard .sb_later').empty();
 
-    var url = items.SB_address + ":" + items.SB_port  + "/api/" + items.SB_key;
-    var error = localStorage.getItem("Sickbeard_error");
+  var url = serviceData.SB.apiUrl;
+  var error = serviceData.SB.error;
 
-    if (error == "true") {
-      $('#sickbeard .error').slideDown('slow');
-    }
-    if (error == "false") {
-      $('#sickbeard .error').slideUp('slow');
-    }
+  if (error == "true") {
+    $('#sickbeard .error').slideDown('slow');
+  }
+  if (error == "false") {
+    $('#sickbeard .error').slideUp('slow');
+  }
 
-    if (localStorage.SickbeardMissedHTML) {
-      $('.sb_missed').append(localStorage.getItem('SickbeardMissedHTML'));
-    }
+  if (serviceData.SB.MissedHTML) {
+    $('.sb_missed').append(serviceData.SB.MissedHTML);
+  }
 
-    if (localStorage.SickbeardTodayHTML) {
-      $('.sb_today').append(localStorage.getItem('SickbeardTodayHTML'));
-    }
+  if (serviceData.SB.TodayHTML) {
+    $('.sb_today').append(serviceData.SB.TodayHTML);
+  }
 
-    if (localStorage.SickbeardSoonHTML) {
-      $('.sb_soon').append(localStorage.getItem('SickbeardSoonHTML'));
-    }
+  if (serviceData.SB.SoonHTML) {
+    $('.sb_soon').append(serviceData.SB.SoonHTML);
+  }
 
-    if (localStorage.SickbeardLaterHTML) {
-      $('.sb_later').append(localStorage.getItem('SickbeardLaterHTML'));
-    }
+  if (serviceData.SB.LaterHTML) {
+    $('.sb_later').append(serviceData.SB.LaterHTML);
+  }
 
-    $('.sb_poster').unveil();
-  });
+  $('.sb_poster').unveil();
 }
 
 function searchEpisode(clickedObject) {
-  SBChromeData(function(items) {
-    clickedObject.fadeOut(400, function() {
-      clickedObject.next('paper-spinner').attr('active', true);
-    });
+  clickedObject.fadeOut(400, function() {
+    clickedObject.next('paper-spinner').attr('active', true);
+  });
 
-    var url = items.SB_address + ":" + items.SB_port  + "/api/" + items.SB_key;
-    var searchApiUrl = url + "/?cmd=episode.search&tvdbid=" + clickedObject.data('tvdbid') + "&season=" + clickedObject.data('season') + "&episode=" + clickedObject.data('episode');
+  var url = serviceData.SB.apiUrl;
+  var searchApiUrl = url + "/?cmd=episode.search&tvdbid=" + clickedObject.data('tvdbid') + "&season=" + clickedObject.data('season') + "&episode=" + clickedObject.data('episode');
 
-    $.ajax({
-      url: searchApiUrl,
-      success: function(data) {
-        if (data.result == "failure") {
-          clickedObject.attr('icon', 'error');
-          clickedObject.attr('title', data.message);
-        } else {
-          clickedObject.attr('icon', 'done');
-        }
-        clickedObject.next('paper-spinner').attr('active', false);
-        setTimeout(function() {
-          clickedObject.show();
-        }, 400);
-      },
-      error: function() {
-        console.log("error");
+  $.ajax({
+    url: searchApiUrl,
+    success: function(data) {
+      if (data.result == "failure") {
+        clickedObject.attr('icon', 'error');
+        clickedObject.attr('title', data.message);
+      } else {
+        clickedObject.attr('icon', 'done');
       }
-    });
+      clickedObject.next('paper-spinner').attr('active', false);
+      setTimeout(function() {
+        clickedObject.show();
+      }, 400);
+    },
+    error: function() {
+      console.log("error");
+    }
   });
 }
 
 function markEpisode(clickedObject) {
-  SBChromeData(function(items) {
-    var url = items.SB_address + ":" + items.SB_port  + "/api/" + items.SB_key;
-    var markApiUrl = url + "/?cmd=episode.setstatus&tvdbid=" + clickedObject.data('tvdbid') + "&season=" + clickedObject.data('season') + "&episode=" + clickedObject.data('episode') + "&status=skipped";
+  var url = serviceData.SB.apiUrl;
+  var markApiUrl = url + "/?cmd=episode.setstatus&tvdbid=" + clickedObject.data('tvdbid') + "&season=" + clickedObject.data('season') + "&episode=" + clickedObject.data('episode') + "&status=skipped";
 
-    $.ajax({
-      url: markApiUrl,
-      success: function(data) {
-        console.log(data);
-        if (data.result == "failure") {
-          clickedObject.attr('icon', 'error');
-          clickedObject.attr('title', data.message);
-        } else {
-          clickedObject.attr('icon', 'done-all');
-        }
-      },
-      error: function() {
-        console.log("error");
+  $.ajax({
+    url: markApiUrl,
+    success: function(data) {
+      if (data.result == "failure") {
+        clickedObject.attr('icon', 'error');
+        clickedObject.attr('title', data.message);
+      } else {
+        clickedObject.attr('icon', 'done-all');
       }
-    });
+    },
+    error: function() {
+      console.log("error");
+    }
   });
-}
-
-function SBChromeData(callback) {
-  chrome.storage.sync.get({
-    SB_status: '',
-    SB_address: '',
-    SB_port: '',
-    SB_key: ''
-  }, callback);
 }
