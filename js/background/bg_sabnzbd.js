f5574d5962f5b31fad4fb03b1a0dd9619c9c5572
Johman10 <johveck@gmail.com>
2015-02-01 12:27:08 +0100

    Sabnzbd serviceData using + succes and error message + replacing download status

--- js/background/bg_sabnzbd.js
+++ js/background/bg_sabnzbd.js
@@ -2,60 +2,54 @@
 // http://wiki.sabnzbd.org/api
 
 function getSabnzbdHistory(callback) {
-  SABChromeData(function(items) {
-    var url = items.SAB_address + ":" + items.SAB_port + "/sabnzbd/api?";
-    var historyMode = "mode=history&limit=" + items.SAB_history;
-    var output = "&output=json";
-    var apiKey = "&apikey=" + items.SAB_key;
+  var url = serviceData.SABH.apiUrl;
+  var historyMode = "&mode=history&limit=" + serviceData.SABH.length;
+  var output = "&output=json";
 
-    $.ajax({
-      url: url + historyMode + output + apiKey,
-      dataType: 'json',
-      async: false,
-      timeout: 3000,
-      success: function(history) {
-        localStorage.setItem("SabnzbdHistory", JSON.stringify(history));
-        localStorage.setItem("SabnzbdHistory_error", false);
-      },
-      error: function(xhr, ajaxOptions, thrownError) {
-        console.log(xhr, ajaxOptions, thrownError);
-        localStorage.setItem("SabnzbdHistory_error", true);
-      }
-    });
-
-    if (callback) {
-      callback();
+  $.ajax({
+    url: url + historyMode + output,
+    dataType: 'json',
+    async: false,
+    timeout: 3000,
+    success: function(history) {
+      localStorage.setItem("SabnzbdHistory", JSON.stringify(history));
+      localStorage.setItem("SabnzbdHistory_error", false);
+    },
+    error: function(xhr, ajaxOptions, thrownError) {
+      console.log(xhr, ajaxOptions, thrownError);
+      localStorage.setItem("SabnzbdHistory_error", true);
     }
   });
+
+  if (callback) {
+    callback();
+  }
 }
 
 function getSabnzbdQueue(callback) {
-  SABChromeData(function(items) {
-    var url = items.SAB_address + ":" + items.SAB_port + "/sabnzbd/api?";
-    var queueMode = "mode=queue";
-    var output = "&output=json";
-    var apiKey = "&apikey=" + items.SAB_key;
+  var url = serviceData.SABQ.apiUrl;
+  var queueMode = "&mode=queue";
+  var output = "&output=json";
 
-    $.ajax({
-      url: url + queueMode + output + apiKey,
-      dataType: 'json',
-      async: false,
-      timeout: 3000,
-      success: function(queue) {
-        localStorage.setItem("SabnzbdQueue", JSON.stringify(queue));
-        localStorage.setItem("SabnzbdQueue_error", false);
-      },
-      error: function(xhr, ajaxOptions, thrownError) {
-        console.log(xhr, ajaxOptions, thrownError);
-        localStorage.setItem("SabnzbdQueue_error", true);
-      }
-    });
-
-    sabHTML();
-    if (callback) {
-      callback();
+  $.ajax({
+    url: url + queueMode + output,
+    dataType: 'json',
+    async: false,
+    timeout: 3000,
+    success: function(queue) {
+      localStorage.setItem("SabnzbdQueue", JSON.stringify(queue));
+      localStorage.setItem("SabnzbdQueue_error", false);
+    },
+    error: function(xhr, ajaxOptions, thrownError) {
+      console.log(xhr, ajaxOptions, thrownError);
+      localStorage.setItem("SabnzbdQueue_error", true);
     }
   });
+
+  sabHTML();
+  if (callback) {
+    callback();
+  }
 }
 
 function sabHTML() {
@@ -63,8 +57,8 @@ function sabHTML() {
       history = '<h2>History</h2>',
       queue = '<h2>Queue</h2>';
 
-  if (localStorage.SabnzbdQueue) {
-    var queueJson = JSON.parse(localStorage.getItem('SabnzbdQueue')),
+  if (serviceData.SABQ.JSON) {
+    var queueJson = serviceData.SABQ.JSON,
         percentage = '',
         timeLeft = '';
 
@@ -101,11 +95,33 @@ function sabHTML() {
     localStorage.setItem('SabnzbdStatusHTML', status);
   }
 
-  if (localStorage.SabnzbdHistory) {
-    var historyJson = JSON.parse(localStorage.getItem('SabnzbdHistory'));
+  if (serviceData.SABH.JSON) {
+    var historyJson = serviceData.SABH.JSON;
 
     $.each(historyJson.history.slots, function(i, hItem) {
-      history += '<core-item label="' + hItem.name + '"></core-item>';
+      console.log(hItem);
+      if (hItem.fail_message === "") {
+        history +=
+          '<core-item class="sab_item_container">' +
+            '<div class="sab_item_name">' +
+              hItem.name +
+            '</div>' +
+            '<div class="sab_item_error">' +
+              hItem.status +
+            '</div>' +
+          '</core-item>';
+      }
+      else {
+        history +=
+        '<core-item class="sab_item_container">' +
+          '<div class="sab_item_name">' +
+            hItem.name +
+          '</div>' +
+          '<div class="sab_item_error">' +
+            hItem.fail_message +
+          '</div>' +
+        '</core-item>';
+      }
     });
 
     if (historyJson.history.slots.length < 1) {
@@ -114,14 +130,4 @@ function sabHTML() {
 
     localStorage.setItem('SabnzbdHistoryHTML', history);
   }
-}
-
-function SABChromeData(callback) {
-  chrome.storage.sync.get({
-    SAB_address: '',
-    SAB_port: '',
-    SAB_key: '',
-    SAB_history: '',
-    SAB_status: ''
-  },callback);
 }
--- js/sabnzbd.js
+++ js/sabnzbd.js
@@ -2,37 +2,35 @@
 // ttp://wiki.sabnzbd.org/api
 
 // "media.list" lists all movies, "data.movies[i].status" returns the status of the movie
-$(document).ready(function() {
-  SABChromeData(function(items) {
-    if (items.SAB_status === true) {
-      sabShowData();
+serviceDataDone.done(function() {
+  if (serviceData.SABQ.status) {
+    window[serviceData.SABQ.feFunctionName]();
 
-      $('.refresh_sab').click(function() {
-        if ($('#sabnzbd .error:visible')) {
-          $('#sabnzbd .error:visible').slideUp(400);
-        }
-        $('.refresh_sab').fadeOut(400, function() {
-          $('.loading_sab').attr('active', true);
-          chrome.runtime.getBackgroundPage(function(backgroundPage) {
-            backgroundPage.getSabnzbdHistory(function() {
-              backgroundPage.getSabnzbdQueue(function() {
-                $('.loading_sab').attr('active', false);
-                setTimeout(function() {
-                  $('.refresh_sab').fadeIn(400);
-                }, 400);
-              });
+    $('.refresh_sab').click(function() {
+      if ($('#sabnzbd .error:visible')) {
+        $('#sabnzbd .error:visible').slideUp(400);
+      }
+      $('.refresh_sab').fadeOut(400, function() {
+        $('.loading_sab').attr('active', true);
+        chrome.runtime.getBackgroundPage(function(backgroundPage) {
+          backgroundPage.getSabnzbdHistory(function() {
+            backgroundPage.getSabnzbdQueue(function() {
+              $('.loading_sab').attr('active', false);
+              setTimeout(function() {
+                $('.refresh_sab').fadeIn(400);
+              }, 400);
             });
           });
         });
       });
+    });
 
-      $('#sabnzbd core-toolbar a').attr('href', items.SAB_address + ':' + items.SAB_port);
+    $('#sabnzbd core-toolbar a').attr('href', serviceData.SABQ.url);
 
-      $('#sabnzbd, .sabnzbd_info').show();
-      $('body').width($('body').width() + $('#sabnzbd').width());
-      $('.bottom_bar_container').width($('.panel_container').width());
-    }
-  });
+    $('#sabnzbd, .sabnzbd_info').show();
+    $('body').width($('body').width() + $('#sabnzbd').width());
+    $('.bottom_bar_container').width($('.panel_container').width());
+  }
 });
 
 function sabShowData() {
@@ -40,8 +38,8 @@ function sabShowData() {
   $('.queue').empty();
   $('.history').empty();
 
-  var queueError = localStorage.getItem('SabnzbdQueue_error');
-  var historyError = localStorage.getItem('SabnzbdHistory_error');
+  var queueError = serviceData.SABQ.error;
+  var historyError = serviceData.SABH.error;
 
   if (queueError == "true" || historyError == "true") {
     $('#sabnzbd .error').slideDown('slow');
@@ -50,17 +48,7 @@ function sabShowData() {
     $('#sabnzbd .error').slideUp('slow');
   }
 
-  $('#sabnzbd .status').append(localStorage.getItem('SabnzbdStatusHTML'));
-  $('#sabnzbd .queue').append(localStorage.getItem('SabnzbdQueueHTML'));
-  $('#sabnzbd .history').append(localStorage.getItem('SabnzbdHistoryHTML'));
-}
-
-function SABChromeData(callback) {
-  chrome.storage.sync.get({
-    SAB_address: '',
-    SAB_port: '',
-    SAB_key: '',
-    SAB_history: '',
-    SAB_status: ''
-  },callback);
+  $('.bottom_bar_container .sabnzbd_info').append(serviceData.SABQ.downloadStatus);
+  $('#sabnzbd .queue').append(serviceData.SABQ.HTML);
+  $('#sabnzbd .history').append(serviceData.SABH.HTML);
 }
