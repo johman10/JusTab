ab684a6492081f9a2b9244b861b2bc771a917db1
Johman10 <johveck@gmail.com>
2015-02-08 02:47:13 +0100

    Scrollheader improvements

--- js/modules/header.js
+++ js/modules/header.js
@@ -1,22 +1,26 @@
 $(document).ready(function() {
-  $('.panel_content').on('scroll', function(e) {
+  $('.panel_content').scroll(function(e) {
+    var scrollTop = e.target.scrollTop;
+    var panel_header = $(this).prev('.panel_header');
+    var topBar = panel_header.find('.top');
+    var panelBackground = panel_header.find('.panel_header_background2');
+    var opacity = 1-(scrollTop*(1/64));
+
     if (e.target.scrollTop < 64) {
-      var scrollTop = e.target.scrollTop;
-      var panel_header = $(this).prev('.panel_header');
-      var topBar = panel_header.find('.top');
-      var panelBackground = panel_header.find('.panel_header_background2');
-      var opacity = 1-(scrollTop*(1/64));
       panel_header.height(128-scrollTop);
       topBar.height(64-scrollTop);
       topBar.css('opacity', opacity);
       panelBackground.css('opacity', opacity);
-      if (opacity < 0.1) {
-        topBar.hide();
-        panelBackground.hide();
-      } else {
-        topBar.show();
-        panelBackground.show();
-      }
+    }
+
+    if (scrollTop > 64) {
+      panel_header.height(64);
+      topBar.height(0);
+      topBar.hide();
+      panelBackground.hide();
+    } else {
+      topBar.show();
+      panelBackground.show();
     }
   });
 });
