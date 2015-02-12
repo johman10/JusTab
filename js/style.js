344f1e22e8f9e490e74bca547c92233be692e318
Johman10 <johveck@gmail.com>
2015-02-12 11:27:40 +0100

    Remove unneeded code for panel_headers Leftovers from polymer

--- js/style.js
+++ js/style.js
@@ -3,24 +3,6 @@ $(document).ready(function() {
   $('body').width(panelWidth);
   $('img').attr('draggable', false);
 
-  $('.panel').on('core-header-transform', function(e) {
-    var refreshButton = $(this).find('.refresh_button');
-    var refreshLoading = $(this).find('.refresh_loading_icon');
-    var id = $(this).attr('id');
-    var opacity = $('#' + id + ' /deep/ #headerBg').css('opacity');
-    refreshButton.css('opacity', opacity);
-    refreshLoading.css('opacity', opacity);
-
-    if (opacity < 0.01) {
-      refreshButton.hide();
-      refreshLoading.hide();
-    }
-    else {
-      refreshButton.show();
-      refreshLoading.show();
-    }
-  });
-
   $('.settings_button, .error_settings_button').click(function(event) {
     chrome.tabs.create({
       'url': chrome.extension.getURL("options_build.html")
