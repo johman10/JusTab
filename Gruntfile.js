eb9f9027c314615ce68327cac9e4b94d0b11b096
Johman10 <johveck@gmail.com>
2015-03-12 18:05:48 +0100

    Rebuild Gruntfile to remove unneeded processes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,9 @@
 module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-compass');
 
   grunt.initConfig({
-    concurrent: {
-      watch: {
-        tasks: ['watch', 'compass'],
-        options: {
-            logConcurrentOutput: true
-        }
-      }
-    },
-    watch: {
-      html: {
-        files: ['index.html', 'options.html'],
-        tasks: ['shell'],
-        options: {
-          spawn: false,
-        },
-      }
-    },
     compass: {
       dist: {
         options: {
@@ -35,19 +17,8 @@ module.exports = function(grunt) {
           watch: true
         }
       }
-    },
-    shell: {
-      options: {
-          stderr: false
-      },
-      target: {
-          command: [
-            'vulcanize --csp -o build.html index.html',
-            'vulcanize --csp -o options_build.html options.html',
-          ].join('&&')
-      }
     }
   });
 
-  grunt.registerTask('default', 'concurrent');
+  grunt.registerTask('default', 'compass');
 };
