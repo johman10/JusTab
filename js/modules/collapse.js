1f8ae72ecbcc802f3e0a5e51efeb2b0125f061ae
Johman10 <johveck@gmail.com>
2015-03-13 00:36:38 +0100

    Collapse items improvements

--- js/modules/collapse.js
+++ js/modules/collapse.js
@@ -5,27 +5,19 @@ $(document).ready(function() {
     var collapseIcon = clickedItem.find('.core_item_icon');
 
     if (collapseItem.hasClass('core_collapse')) {
-      $('.core_collapse').slideUp('330');
-      $('.core_item').css('background-color', 'transparent');
-      $('.core_item_icon').css('background-color', 'transparent');
-      $('.expand_less_icon').fadeOut(165, function() {
-        $(this).attr('class', 'expand_more_icon');
+      $('.core_item.expanded .core_item_icon').fadeOut(165, function() {
+        $(this).children('.expand_less_icon').attr('class', 'expand_more_icon');
         $(this).fadeIn(165);
       });
-      if (collapseItem.is(":hidden")) {
-        collapseItem.slideDown('330');
-        clickedItem.css('background-color', '#eee');
-        collapseIcon.css('background-color', '#eee');
+      $('.core_item.expanded').not(clickedItem).removeClass('expanded');
+      $('.core_collapse.expanded').not(collapseItem).removeClass('expanded');
+
+      clickedItem.toggleClass('expanded');
+      collapseItem.toggleClass('expanded');
+
+      if (collapseItem.hasClass('expanded')) {
         collapseIcon.fadeOut(165, function() {
           $(this).find('.expand_more_icon').attr('class', 'expand_less_icon');
-          $(this).fadeIn(165);
-        });
-      } else {
-        collapseItem.slideUp('330');
-        clickedItem.css('background-color', 'transparent');
-        collapseIcon.css('background-color', 'transparent');
-        collapseIcon.fadeOut(165, function() {
-          $(this).find('.expand_less_icon').attr('class', 'expand_more_icon');
           $(this).fadeIn(165);
         });
       }
