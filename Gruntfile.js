7515023d62fef4bec561c9c3a96ffc82ed7ef64d
Johman10 <johveck@gmail.com>
2015-03-21 12:56:10 +0100

    Update for grunt with dist function + reordering folder structure + adding dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,23 +2,88 @@ module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
 
   grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-compress');
 
   grunt.initConfig({
     compass: {
-      dist: {
+      dev: {
         options: {
-          sassDir: 'style/sass',
-          specify: 'style/sass/main.scss',
-          cssDir: 'style/css',
-          imagesDir: 'img',
-          javascriptsDir: 'js',
-          fontsDir: 'style/fonts',
+          sassDir: 'dev/style/sass',
+          specify: 'dev/style/sass/main.scss',
+          cssDir: 'dev/style/css',
+          imagesDir: 'dev/img',
+          javascriptsDir: 'dev/js',
+          fontsDir: 'dev/style/fonts',
           outputStyle: 'compressed',
           watch: true
         }
+      },
+      dist: {
+        options: {
+          sassDir: 'dev/style/sass',
+          specify: 'dev/style/sass/main.scss',
+          cssDir: 'dist/style/css',
+          imagesDir: 'dist/img',
+          javascriptsDir: 'dist/js',
+          fontsDir: 'dist/style/fonts',
+          outputStyle: 'compressed'
+        }
+      }
+    },
+
+    uglify: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'dev/js',
+          src: '**/*.js',
+          dest: '../dist/js'
+        }]
+      }
+    },
+
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'dev/',
+          src: [
+            'img/**',
+            'bower_components/jquery/dist/jquery.min.js',
+            'bower_components/moment/min/moment.min.js',
+            'bower_components/unveil/jquery.unveil.min.js',
+            'style/fonts/**',
+            'index.html',
+            'options.html',
+            'background.html',
+            'manifest.json'
+          ],
+          dest: 'dist/'
+        }]
+      }
+    },
+
+    compress: {
+      dist: {
+        options: {
+          archive: 'dist.zip'
+        },
+        files: [{
+          expand: true,
+          src: ['dist/**'],
+          dest: '.'
+        }]
       }
     }
   });
 
-  grunt.registerTask('default', 'compass');
+  grunt.registerTask('default', 'compass:dev');
+  grunt.registerTask('dist', [
+    'copy:dist',
+    'compass:dist',
+    'uglify:dist',
+    'compress:dist'
+  ]);
 };
