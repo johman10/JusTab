7b0c9e2765a7323773def58de396136e2462396b
Johan van Eck <johveck@gmail.com>
2017-10-21 13:27:37 +0200

    Change how transmission sorts between Queue and History

--- js/background/v-transmission.js
+++ js/background/v-transmission.js
@@ -60,7 +60,7 @@ export default {
     },
 
     transmissionQueueComponents (torrents) {
-      torrents = torrents.filter(torrent => !torrent.isFinished);
+      torrents = torrents.filter(torrent => torrent.percentDone !== 1);
 
       const queueComponents = [];
       queueComponents.push(this.transmissionSubheaders('Queue'));
@@ -86,7 +86,7 @@ export default {
     },
 
     transmissionHistoryComponents (torrents) {
-      torrents = torrents.filter(torrent => torrent.isFinished);
+      torrents = torrents.filter(torrent => torrent.percentDone === 1);
       torrents.sort((torrentA, torrentB) => torrentB.addedDate - torrentA.addedDate);
       torrents = torrents.splice(0, 25);
 
