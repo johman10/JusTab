b133979b6692b4fecd8aca67898321d2645f938a
Johan van Eck <johveck@gmail.com>
2017-12-29 18:13:17 +0100

    Fix sortedServices to prevent new services from not showing

--- store/getters.js
+++ store/getters.js
@@ -4,14 +4,17 @@ export const activeServices = (state, getters) => {
 
 export const sortedServices = (state) => {
   if (!state.services.length) return [];
-  var serviceOrder = localStorage.getItem('serviceOrder');
+  const serviceOrder = localStorage.getItem('serviceOrder');
   if (!serviceOrder) return state.services;
-  var serviceIds = state.services.map(service => service.id);
-  var sortedServices = [];
-  var order = localStorage.getItem('serviceOrder').split(',');
-  order.forEach((id) => {
-    id = parseInt(id);
-    sortedServices.push(state.services[serviceIds.indexOf(id)]);
+  let sortedIds = serviceOrder.split(',').map(id => parseInt(id));
+  if (sortedIds.length < state.services.length) {
+    const servicesIds = state.services.map(service => service.id);
+    const missingIds = servicesIds.filter(i => sortedIds.indexOf(i) < 0);
+    sortedIds = sortedIds.concat(missingIds);
+  }
+  const sortedServices = sortedIds.map((id) => {
+    return state.services.find(service => service.id === parseInt(id));
   });
+
   return sortedServices;
 };
