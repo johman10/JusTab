90b51608383d447ea9ae16b6c71afc5929240d9d
Johan van Eck <johan.van.eck@zalando.fi>
2021-07-28 16:55:29 +0300

    Fix

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,24 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
+  testEnvironment: "jsdom",
   verbose: true,
-  moduleFileExtensions: ['js', 'vue'],
-  moduleDirectories: ['node_modules'],
-  testMatch: ['**/__tests__/**/?(*.)+(spec|test).js'],
-  testPathIgnorePatterns: ['./node_modules'],
-  transformIgnorePatterns: ['./node_modules'],
+  moduleFileExtensions: ["js", "vue"],
+  moduleDirectories: ["node_modules"],
+  testMatch: ["**/__tests__/**/?(*.)+(spec|test).js"],
+  testPathIgnorePatterns: ["./node_modules"],
+  transformIgnorePatterns: ["./node_modules"],
   transform: {
-    '^.+\\.js$': 'babel-jest',
-    '^.+\\.vue$': 'vue-jest'
+    "^.+\\.js$": "babel-jest",
+    "^.+\\.vue$": "vue-jest",
   },
   moduleNameMapper: {
-    '^store/(.*)$': path.join(__dirname, '/store/$1'),
-    '^modules/(.*)$': path.join(__dirname, '/js/modules/$1'),
-    '^img/(.*)$': path.join(__dirname, '/img/$1'),
-    '^js/(.*)$': path.join(__dirname, '/js/$1'),
-    '^css/(.*)$': path.join(__dirname, '/style/sass/$1'),
-    '^components/(.*)$': path.join(__dirname, '/components/$1'),
-    '^test/(.*)$': path.join(__dirname, '/__tests__/$1'),
-  }
+    "^store/(.*)$": path.join(__dirname, "/store/$1"),
+    "^modules/(.*)$": path.join(__dirname, "/js/modules/$1"),
+    "^img/(.*)$": path.join(__dirname, "/img/$1"),
+    "^js/(.*)$": path.join(__dirname, "/js/$1"),
+    "^css/(.*)$": path.join(__dirname, "/style/sass/$1"),
+    "^components/(.*)$": path.join(__dirname, "/components/$1"),
+    "^test/(.*)$": path.join(__dirname, "/__tests__/$1"),
+  },
 };
