44356512d1617247f7fdf09d8cec74326a580a89
Johan van Eck <johveck@gmail.com>
2021-11-04 19:58:00 +0200

    Fix Github description

--- js/background/v-github.js
+++ js/background/v-github.js
@@ -2,40 +2,46 @@ import ajax from 'modules/ajax';
 
 export default {
   computed: {
-    githubService () {
+    githubService() {
       return this.services.find(s => s.id === 8);
-    }
+    },
   },
 
   methods: {
-    github () {
+    github() {
       localStorage.setItem('githubError', false);
       return this.githubRequest()
         .then(this.githubBuildJson)
         .then(this.githubComponents)
-        .catch((error) => {
+        .catch(error => {
           if (error) console.error(error); // eslint-disable-line no-console
           localStorage.setItem('githubError', true);
         });
     },
 
-    githubRequest () {
+    githubRequest() {
       var url = `https://github.com/trending?since=${this.githubService.time}`;
       return ajax('GET', url);
     },
 
-    githubBuildJson (html) {
-      return new Promise((resolve) => {
+    githubBuildJson(html) {
+      return new Promise(resolve => {
         const parsedDom = document.createElement('html');
         parsedDom.innerHTML = html;
 
         const repoListItems = Array.from(parsedDom.querySelectorAll('article'));
-        const repos = repoListItems.map((repoListItem) => {
-          const languageElement = repoListItem.querySelector('[itemprop="programmingLanguage"]');
-          const starGazersLink = repoListItem.querySelector('[href*="/stargazers"]');
-          const repoLink = repoListItem.querySelector('a:not([href*="login"]):not([aria-label*="Sponsor @"])');
+        const repos = repoListItems.map(repoListItem => {
+          const languageElement = repoListItem.querySelector(
+            '[itemprop="programmingLanguage"]',
+          );
+          const starGazersLink = repoListItem.querySelector(
+            '[href*="/stargazers"]',
+          );
+          const repoLink = repoListItem.querySelector(
+            'a:not([href*="login"]):not([aria-label*="Sponsor @"])',
+          );
           const repoInfo = repoLink.innerText.trim().replace(/[ \n]/g, '');
-          const descriptionElement = repoListItem.querySelector('p.color-text-secondary');
+          const descriptionElement = repoListItem.querySelector('p');
           const title = repoInfo.split('/')[1];
           const owner = repoInfo.split('/')[0];
 
@@ -44,8 +50,10 @@ export default {
             stars: starGazersLink ? starGazersLink.innerText.trim() : 0,
             title,
             owner,
-            description: descriptionElement ? descriptionElement.innerText.trim() : null,
-            url: `https://github.com/${owner}/${title}`
+            description: descriptionElement
+              ? descriptionElement.innerText.trim()
+              : null,
+            url: `https://github.com/${owner}/${title}`,
           };
         });
 
@@ -53,10 +61,10 @@ export default {
       });
     },
 
-    githubComponents (json) {
+    githubComponents(json) {
       let components = [];
 
-      json.forEach((repo) => {
+      json.forEach(repo => {
         let extraTitle = '';
         if (repo.language) {
           extraTitle += `${repo.language} - `;
@@ -69,8 +77,8 @@ export default {
             url: repo.url,
             title: `${repo.owner}/${repo.title}`,
             extraTitle,
-            subtitle: repo.description
-          }
+            subtitle: repo.description,
+          },
         });
       });
 
@@ -78,12 +86,12 @@ export default {
         components.push({
           name: 'v-panel-item',
           props: {
-            title: 'There are no repositories to show at the moment.'
-          }
+            title: 'There are no repositories to show at the moment.',
+          },
         });
       }
 
       localStorage.setItem('githubComponents', JSON.stringify(components));
-    }
-  }
+    },
+  },
 };
